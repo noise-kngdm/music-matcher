99247ccafc8558fe0e2f1b52b7f1c47aff36ff61
Joaquín García Venegas <joaquingv12@gmail.com>
2021-10-09 17:22:31 +0200

    Added better classes documentation

--- music/song.py
+++ music/song.py
@@ -1,8 +1,23 @@
 class Song:
-    '''A class representing the information of a song'''
+    '''A class representing the information of a song '''
     def __init__(self, title, music_genre, song_length, artist, related_songs):
+        ''' Song class constructor
+            
+            Attributes
+            ----------
+            title : string
+                    It's the title of the song.
+            music_genre : string
+                    Music genre which it belongs.
+            song_length : float 
+                    The time it takes to play.
+            artist : string
+                    The singer or band who composed the song.
+            related_songs: list of string
+                    List of similar songs.
+        '''
         self.title = title
         self.music_genre = music_genre
         self.song_length = song_length
-        self.author = artist
+        self.artist = artist
         self.related_songs = related_songs
--- user_information/profile.py
+++ user_information/profile.py
@@ -1,6 +1,23 @@
 class Profile:
     '''A class representing an user of the application'''
     def __init__(self, age, name, gender, favourite_music_genre, favourite_artists, music_history):
+        ''' Profile class constructor
+            
+            Attributes
+            ----------
+            age : int
+                User's age.
+            name : string
+                User's name.
+            gender : string
+                User's gender.
+            favourite_music_genre : string
+                User's favourite music genre.
+            favourite_artists : list of string
+                User's favourite artists.
+            music_history : MusicHistory object
+                It will contains all the user music history.
+        '''
         self.age = age
         self.name = name
         self.gender = gender
