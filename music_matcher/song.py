ac2760195becc548b03ac7722a3e74b84848e89e
Gonzalo Abril Paniza <gonzalo.abril@wazuh.com>
2021-11-05 09:48:00 +0100

    Add `data/music_genres.yaml` file (#20)

--- music_matcher/song.py
+++ music_matcher/song.py
@@ -3,7 +3,7 @@ A module that represents all information related to music metadata.
 """
 class Song:
     '''A class representing the information of a song. '''
-    def __init__(self, title: str, music_genre: str, artist: str, year: int):
+    def __init__(self, title: str, music_genre: str, subgenre: str, artist: str, year: int):
         '''
         Song class constructor.
 
@@ -13,6 +13,8 @@ class Song:
             It's the title of the song.
         music_genre : str
             The song's music genre.
+        subgenre : str
+            The song's music subgenre. It must be one of the music subgenres defined for that music genre in the `music_genres.yaml` file.
         artist : str
             The singer or band who composed the song.
         year : int
@@ -20,5 +22,6 @@ class Song:
         '''
         self.title = title
         self.music_genre = music_genre
+        self.subgenre = subgenre
         self.artist = artist
         self.year = year
