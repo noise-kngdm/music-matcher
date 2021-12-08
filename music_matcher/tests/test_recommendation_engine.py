f3614a8cd6d260b72680c249838913512354a694
Gonzalo Abril <gonzalo.abril@wazuh.com>
2021-12-08 21:59:01 +0100

    Change constants to uppercase (#2)

--- music_matcher/tests/test_recommendation_engine.py
+++ music_matcher/tests/test_recommendation_engine.py
@@ -4,8 +4,8 @@ from assertpy import assert_that
 import music_matcher.recommendation_engine as rec
 
 
-genres_path = 'music_matcher/data/music_genres.yaml'
-wrong_genres_path = 'music_matcher/tests/data/wrong_genres.yaml'
+GENRES_PATH = 'music_matcher/data/music_genres.yaml'
+WRONG_GENRES_PATH = 'music_matcher/tests/data/wrong_genres.yaml'
 
 
 @pytest.mark.parametrize('user_names', [
@@ -14,15 +14,15 @@ wrong_genres_path = 'music_matcher/tests/data/wrong_genres.yaml'
 ])
 def test_recommendation_init(songs, users, user_names):
     users = [users(username, songs) for username in user_names]
-    engine = rec.RecommendationEngine(genres_path, users)
+    engine = rec.RecommendationEngine(GENRES_PATH, users)
     assert_that(engine).is_type_of(rec.RecommendationEngine)
 
 
 @pytest.mark.parametrize('genres_path,user_names,expected_exception', [
-    (genres_path, ['lucia'], rec.RecommendationError),
+    (GENRES_PATH, ['lucia'], rec.RecommendationError),
     (None, ['lucia', 'luis', 'daniel'], rec.RecommendationTypeError),
     ('unexisting_file', ['lucia', 'luis', 'daniel'], rec.RecommendationError),
-    (wrong_genres_path, ['lucia', 'luis', 'daniel'], rec.RecommendationParsingError)
+    (WRONG_GENRES_PATH, ['lucia', 'luis', 'daniel'], rec.RecommendationParsingError)
 ])
 def test_recommendation_init_ko(songs, users, genres_path, user_names,
                                 expected_exception):
