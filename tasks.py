3237dd8d6551a3134738a72d51c5c97a36cb7b9e
Gonzalo Abril <gonzalo.abril@wazuh.com>
2021-12-12 17:42:51 +0100

    Add tests running to invoke tasks.
    Kept working on #2

--- tasks.py
+++ tasks.py
@@ -6,6 +6,19 @@ To list all the available tasks run: 'inv --list'
 """
 from invoke import task
 
+
+def run(ctx, command: str, pty: bool = True):
+    """
+    Runs a command inside poetry's venv.
+
+    Parameters
+    ----------
+    command : str
+        The command that will be ran.
+    """
+    ctx.run(f'poetry run {command}', pty=pty)
+
+
 @task(help={'style': 'Analyze code style too'})
 def check(ctx, style=False):
     """
@@ -17,4 +30,21 @@ def check(ctx, style=False):
         If code style will be checked too.
     """
     pylint_check = ['music_matcher', 'tasks.py']
-    ctx.run('pylint ' + ('-E ' if not style else '') + ' '.join(pylint_check))
+    run(ctx, 'pylint ' + ('-E ' if not style else '') + ' '.join(pylint_check))
+
+
+@task(help={'keyword': 'Filter the test list.',
+            'capture_output': 'Print stdout and stderr.'})
+def test(ctx, keyword='', capture_output=False):
+    """
+    Run the unit tests.
+
+    Parameters
+    ----------
+    keyword : str
+        The keyword that will be used to select which tests will be ran. If its
+        empty, all tests will be ran.
+    capture_output : bool
+        If set to True, pytest will capture and show stdout and stderr too.
+    """
+    run(ctx, f'poetry run pytest {"-k keyword" if keyword else ""} {""if not capture_output else "-rP"}')
